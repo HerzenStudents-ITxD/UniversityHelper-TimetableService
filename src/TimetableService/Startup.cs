c859c57da83eb181bb52c6dc5fec479066de69f9
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-05-17 02:00:36 +0300

    Update Startup.cs

--- src/TimetableService/Startup.cs
+++ src/TimetableService/Startup.cs
@@ -69,8 +69,7 @@ public class Startup : BaseApiInfo
             builder
               .AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
           });
     });
 
