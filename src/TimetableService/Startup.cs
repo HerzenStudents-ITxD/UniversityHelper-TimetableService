419c3c327dc59265c43c08251f6d0cfa7af3817f
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-05-16 03:12:03 +0300

    Update Startup.cs

--- src/TimetableService/Startup.cs
+++ src/TimetableService/Startup.cs
@@ -66,10 +66,13 @@ public class Startup : BaseApiInfo
         CorsPolicyName,
           builder =>
           {
-            builder
-              .AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+          builder
+            .WithOrigins(
+              "http://localhost:5173",
+              "https://itvd.online")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
           });
     });
 
